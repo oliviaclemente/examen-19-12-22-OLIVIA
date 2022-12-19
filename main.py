1c93d973238ccdcd81048d2c134d9ad2c6fd71e5
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 08:49:19 +0000

    c

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 from datetime import date 
-from dateutil.relativedelta import relatividelta
-
-
-def edad(nac):
-  hoy = datetime.date.today()
-  if hoy<nac:
-    print("No es prosibe!!")
-  else:
-    año=nac.year
-    mes= nac.month
-    dia= nac.day
-    fecha= nac
-    edad= 0
-    while fecha< hoy:
-      edad += 1
-      fecha = datetime.date(año + edad, mes, dia)
-    print("Tengo",(edad-1),"años")
-  return edad
-    
-edad(datetime.date(10, 03, 2004))
+from dateutil.relativedelta import relativedelta
 
 def c_e(f_n):
   edad= date.today().year - f_n.year
-  cumple= f_n + relativedelta    
+  cumple= f_n + relativedelta(years=edad)
+  if cumple > date.today():
+    edad= edad -1
+  return edad
+
+c_e(date(2004,10,10))
+
+def c_e(f_n):
+  today= fecha.today()
+  try:
+    cumple= naci.replace(año= today.year)
+  excepy ValueError:
+    cumple= naci.replace( año = today.year)
+  if cumple > today:
+    return today.yaer - naci.year -1
+  else:
+    returm today.year
