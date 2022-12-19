1a75a4a849ce346a2090e3413b3abf78df3afced
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 09:15:15 +0000

    c

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+lista=[]
 def do_math():
-  lista[]
   lista.sort
   lista1= lista.replace("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-return do_math
+  
+  return do_math
