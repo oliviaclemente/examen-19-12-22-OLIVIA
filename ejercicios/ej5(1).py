d9876ca122f89b026820b3b6e9dcb09e18df83c6
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 10:03:28 +0000

    c

--- ejercicios/ej5(1).py
+++ ejercicios/ej5(1).py
@@ -2,3 +2,8 @@ num= 6
 for i in range(num):
   print('_'+ '#' * (num-i-1)+'_'*(2* i +1) + (num-i-1)* '#'+ '_' )
 print("")
+
+
+num=9
+for i in range(num):
+  print('_'+ '#' * (num-i-1)+'_'*(2* i +1) + (num-i-1)* '#'+ '_' )
