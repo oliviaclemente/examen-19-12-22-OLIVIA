13a518f246c1da09430982c148ebfde04d9c3a53
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 09:19:54 +0000

    c

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-lista=[]
-def do_math():
-  lista.sort
-  lista1= lista.replace("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+texto= "24z6 1x23 y369 89a 900b"
+lista= texto.slipt("")
+print(lista)
+#lista.sort
+#lista1= lista.replace("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
   
-  return do_math
+#print(lista)
