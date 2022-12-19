3739d274ac8645b40e18cf2466f86ac2ab08270e
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 07:52:03 +0000

    c

---
+++ ejercicios/ej2.py
@@ -0,0 +1,6 @@
+def simplify(x,y):
+  return 3*x -y*x + 2*x*y -x
+
+x= float(intput("Valor de x:"))
+y= float(intput("Valor de y:"))
+print(simplify(x,y))
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def simplify(x,y):
-  return 3*x -y*x + 2*x*y -x
+max_num=999
+unidades= ("cero","uno";"dos","tres","cuatro","cinco","seis","siete","ocho","nueve")
 
-x=float(intput())
+def num(numero):
+  ne=int(numero)
+  if ne>max_num:
+    raise OverflowError("Numero muy alto")
+  if ne<0:
+    raise OverflowError("Numero demasiado bajo")
+  if ne>9:
+    
+    
+    
