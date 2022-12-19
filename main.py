ae1a0ee7146ae83b68b4d3bd86bc66a919989ebc
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 08:33:49 +0000

    c

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import fechanac
 
 def edad(nac):
   hoy = fechanac.date.today()
+  if hoy<nac:
+    print("No es prosibe!!")
+  else:
+    año=nac.year
+    mes= nac.month
+    dia= nac.day
+    fecha= nac
+    edad= 0
+    while fecha< hoy:
+      
+    
