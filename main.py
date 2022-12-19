fe52785685dbba65e3d193432fce8159a73556b4
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 08:50:29 +0000

    c

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from datetime import date 
 from dateutil.relativedelta import relativedelta
 
-def c_e(f_n):
-  edad= date.today().year - f_n.year
-  cumple= f_n + relativedelta(years=edad)
-  if cumple > date.today():
-    edad= edad -1
-  return edad
 
-c_e(date(2004,10,10))
 
 def c_e(f_n):
-  today= fecha.today()
+  today= date.today()
   try:
     cumple= naci.replace(año= today.year)
   excepy ValueError:
@@ -19,4 +12,5 @@ def c_e(f_n):
   if cumple > today:
     return today.yaer - naci.year -1
   else:
-    returm today.year
+    returm today.year - naci.year
+print(calculateEdad(date(2004, 10, 2), "years"))
