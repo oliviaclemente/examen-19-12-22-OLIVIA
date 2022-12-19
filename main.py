ebcb4c63544df97c6a730bce65cd8b0171d85d4e
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-19 08:15:40 +0000

    cc

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ while (i<n):
   aleatorio=random.randrange(1,9)
   print(aleatorio)
   if(aleatorio==1):
-    
+    x=x+1
+    y=y+2
+    print("primera pos caballo:(",x,",","")
+  if(aleatorio==2):
+    if(x+1<9 and y+2<9):
+      x= x+2
+      y= y+1
+      print()
